f22314b79ef3f510a02b6abbc6895331aec36ebf
Kttra <100814612+Kttra@users.noreply.github.com>
2022-05-08 12:57:31 -0700

    Made text output more cleaner

--- translateLine.ps1
+++ translateLine.ps1
@@ -97,6 +97,7 @@ $Uri = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&t
 # Get the response from the web request, then throw a bunch of regex at it to clean it up.
 $RawResponse = (Invoke-WebRequest -Uri $Uri -Method Get).Content
 $CleanResponse = $RawResponse -split '\\r\\n' -replace '^(","?)|(null.*?\[")|\[{3}"' -split '","'
+$CleanResponse = $CleanResponse -replace "\]|\[|_en_2022q1.md|\,|", ""
 #Selecting every odd line and adding it to the $Translation list, we recreate the full translated text.
 $LineNumber = 0
 foreach ($Line in $CleanResponse) {
